62b6c20cd2ddc734a5ce2610bf042c90bc667396
victor.mesquini <victor.mesquini@ingaia.com.br>
2020-06-15 12:11:59 -0300

    new router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,10 @@ import categoryRouter from './category.routes';
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+  return res.json({ status: true });
+});
+
 routes.use('/transactions', transactionRouter);
 routes.use('/categories', categoryRouter);
 
